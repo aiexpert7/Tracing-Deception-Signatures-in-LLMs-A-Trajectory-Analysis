9e0f565e6da42eae4609845f560479a27eaec058
aiexpert7 <airesearchexpert7@gmail.com>
2025-10-09 16:39:20 -0400

    Create __init__.py

---
+++ llm_trajectory_toolkit/analysis/__init__.py
@@ -0,0 +1,11 @@
+from .statistical import StatisticalAnalyzer, PairedTestResult, compare_truthful_deceptive
+from .classification import DeceptionClassifier, ClassificationResult, train_deception_classifier
+
+__all__ = [
+    'StatisticalAnalyzer',
+    'PairedTestResult',
+    'compare_truthful_deceptive',
+    'DeceptionClassifier',
+    'ClassificationResult',
+    'train_deception_classifier'
+]
